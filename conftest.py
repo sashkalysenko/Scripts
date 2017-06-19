5b992030b36813c70d151d3340aed74372a575c4
Aleksandr <sashka.lysenko@gmail.com>
2017-06-20 00:26:18 +0300

    bvcxcvb

--- conftest.py
+++ conftest.py
@@ -1,20 +1,26 @@
 import pytest
+import json
+import os.path
+import importlib
+import jsonpickle
 from fixture.application import Application
 
 wd = None
+target = None
 
 
 @pytest.fixture
 def app(request):
     global wd
+    global target
     browser = request.config.getoption("--browser")
-    base_url = request.config.getoption("--baseUrl")
-    if wd is None:
-        wd = Application(browser=browser, base_url=base_url)
-    else:
-        if not wd.is_valid():
-            wd = Application(browser=browser, base_url=base_url)
-    wd.session.ensure_login(username="admin", password="secret")
+    if target is None:
+        config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), request.config.getoption("--target"))
+        with open(config_file) as f:
+            target = json.load(f)
+    if wd is None or not wd.is_valid():
+        wd = Application(browser=browser, base_url=target['baseUrl'])
+    wd.session.ensure_login(username=target['username'], password=target['password'])
     return wd
 
 
@@ -30,4 +36,23 @@ def stop(request):
 
 def pytest_addoption(parser):
     parser.addoption("--browser", action="store", default="chrome")
-    parser.addoption("--baseUrl", action="store", default="http://localhost/addressbook/")
+    parser.addoption("--target", action="store", default="target.json")
+
+
+def pytest_generate_tests(metafunc):
+    for fixture in metafunc.fixturenames:
+        if fixture.startswith("data_"):
+            testdata = load_from_module(fixture[5:])
+            metafunc.parametrize(fixture, testdata, ids=[str(x) for x in testdata])
+        elif fixture.startswith("json_"):
+            testdata = load_from_json(fixture[5:])
+            metafunc.parametrize(fixture, testdata, ids=[str(x) for x in testdata])
+
+
+def load_from_module(module):
+    return importlib.import_module("data.%s" % module).testdata
+
+
+def load_from_json(file):
+    with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), "data/%s.json" % file)) as f:
+        return jsonpickle.decode(f.read())
