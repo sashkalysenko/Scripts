06b3af573ad44888b1190144c0959f7bebcb7f32
Aleksandr <sashka.lysenko@gmail.com>
2017-07-02 19:43:00 +0300

    645

--- check_db_connection.py
+++ check_db_connection.py
@@ -1,12 +1,13 @@
-from fixture.db import DbFixture
+from fixture.orm import ORMFixture
+from model.group import Group
 
 
-db = DbFixture(host='localhost', name='addressbook', user='root', password='')
+db = ORMFixture(host='localhost', name='addressbook', user='root', password='')
 
 try:
-    contacts = db.get_contact_list()
-    for contact in contacts:
-        print(contact)
-    print(len(contacts))
+    l = db.get_contacts_in_group(Group(id='87'))
+    for item in l:
+        print(item)
+    print(len(l))
 finally:
-    db.destroy()
+    pass #db.destroy()
